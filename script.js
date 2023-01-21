41376b321b31806ad3d3ea07eba0365037b6d1f8
Viresh <vireshkoli00@gmail.com>
2023-01-22 00:46:47 +0530

    Browse all images added, Home <-> Search Page Transition Added, REDKing V playlist development started

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 let proname = document.getElementById("proname");
 let information = document.getElementById("information")
+let proname2 = document.getElementById("proname2");
+let information2 = document.getElementById("information2")
+let proname3 = document.getElementById("proname3");
+let information3 = document.getElementById("information3")
 
 function profile(){
     information.classList.toggle("infonone");
+    information2.classList.toggle("infonone");
+    information3.classList.toggle("infonone");
 }
 proname.addEventListener('click', profile);
+proname2.addEventListener('click', profile);
+proname3.addEventListener('click', profile);
 
 
 let head = document.getElementById("head");
 let sub1 = document.getElementById("sub1");
-// let sub2 = document.getElementById("sub2");
-// let sub3 = document.getElementById("sub3");
-// let sub4 = document.getElementById("sub4");
-// let sub5 = document.getElementById("sub5");
-// let sub6 = document.getElementById("sub6");
+let sub2 = document.getElementById("sub2");
+let sub3 = document.getElementById("sub3");
+let sub4 = document.getElementById("sub4");
+let sub5 = document.getElementById("sub5");
+let sub6 = document.getElementById("sub6");
 
 sub1.onmouseover = function(){
     document.body.style.background = 'linear-gradient(180deg, rgb(160, 9, 9) 0%, rgba(0, 0, 0, 1) 20%)';
@@ -68,3 +76,34 @@ sub6.onmouseout = function(){
     document.body.style.background = '';
     head.style.background = '';
 }
+
+// From Home to Search and Search to Home
+let home = document.getElementsByClassName("container");
+let search = document.getElementsByClassName("container2");
+let homebtn = document.getElementsByClassName("li1");
+let searchbtn = document.getElementsByClassName("li2");
+let librarybtn = document.getElementsByClassName("li3");
+let redkingv = document.getElementsByClassName("redkingv");
+
+function searchpage(){
+    home[0].style.display = "none";
+    search[0].style.display = "block";
+    redkingv[0].style.display = "none"
+}
+searchbtn[0].addEventListener("click", searchpage);
+
+function homepage(){
+    home[0].style.display = "block";
+    search[0].style.display = "none";
+    redkingv[0].style.display = "none"
+}
+homebtn[0].addEventListener("click", homepage);
+
+
+let subsection = document.getElementsByClassName("subsection")
+let subsection1 = subsection[0]
+function playredkingv(){
+    home[0].style.display = "none";
+    redkingv[0].style.display = "block";
+}
+subsection1.addEventListener("click", playredkingv);
